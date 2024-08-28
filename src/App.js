4fc78b87786816f201498f6f98ef065ce645f1f7
Eugene Zveringa <zveringa@gmail.com>
2024-08-28 21:08:37 +0100

    Project BLOG setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-camera-retro"></i>
+			<div>456</div>
+		</Div>
+	);
+};
